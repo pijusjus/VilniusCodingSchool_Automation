7de9e5c134c36495db2c80c35eff61dbd787a1c9
pijusjus <kristupas.jusys@gmail.com>
2023-02-10 17:28:27 +0200

    Done with Homework 01

---
+++ Calculation/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculation
+{
+    public class Calculation
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Deduction(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public static double Division(int a, int b)
+        {
+            return (double)a / b;
+        }
+    }
+}
---
+++ Tests/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Class1
+    {
+        [Test]
+        public void Division993Test()
+        {
+            int expectedResult = 0;
+
+            double actualResult = 995 % 3;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void WendesdayTest()
+        {
+
+            var currentTime = DateTime.Now;
+
+            if (currentTime.DayOfWeek == DayOfWeek.Wednesday)
+            {
+                Assert.IsTrue(true);
+            }
+            else
+            {
+                Assert.IsTrue(false);
+            }
+        }
+
+        [Test]
+        public void ThirteenthHourTest()
+        {
+            var currentTime = DateTime.Now;
+
+
+            if (currentTime.Hour == 13)
+            {
+                Assert.IsTrue(true);
+            }
+            else
+            {
+                Assert.IsTrue(false);
+            }
+        }
+
+        [Test]
+        public void EqualNumbersTest()
+        {
+            int count = 0;
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    count++;
+                }
+            }
+
+            Assert.AreEqual(4, count);
+        }
+    }
+}
